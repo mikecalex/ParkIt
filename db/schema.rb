56290498118aa71226b6dc32612219a72ecdce0c
ianclancy <ianrclancy@gmail.com>
2017-10-17 16:30:46 -0400

    Created reviews table, added reviews to park show page, and wrote applicable tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017142849) do
+ActiveRecord::Schema.define(version: 20171017182408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20171017142849) do
     t.integer "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.text "body"
+    t.bigint "park_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["park_id"], name: "index_reviews_on_park_id"
   end
 
 end
