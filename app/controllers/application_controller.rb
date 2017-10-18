a2b7cda714e5b11a3785f185e1f9008c340c9d7d
mikecalex <mikecalex@gmail.com>
2017-10-18 14:53:50 -0400

    Delete "def delete" and add missing "end"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def delete
-    
-  end
-
   protected
 
   def configure_permitted_parameters
